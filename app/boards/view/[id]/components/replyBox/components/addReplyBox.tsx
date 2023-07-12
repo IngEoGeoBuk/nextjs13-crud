b811f0861776016adfd46102578870daf4b7c359
IngEoGeoBuk <you3667@gmail.com>
2023-07-12 21:55:31 +0900

    refactor: view 모달, 버튼

--- app/boards/view/[id]/components/replyBox/components/addReplyBox.tsx
+++ app/boards/view/[id]/components/replyBox/components/addReplyBox.tsx
@@ -47,7 +47,7 @@ function AddReplyBox({ showReply, setShowReply }: Interface) {
         <div className="items-center justify-between px-3 py-2 border-t dark:border-gray-600">
           <button
             type="submit"
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+            className="btn-primary"
           >
             Add reply
           </button>
